47a84ef1ef525a8c487057f2d4a975b4c8731257
DavidSchimmel <david.schimmel@gmx.de>
2023-11-02 22:51:35 +0100

    Update: Add advanced text template for qualtrics survey text export

--- create_qualtrics_input_file.py
+++ create_qualtrics_input_file.py
@@ -20,23 +20,72 @@ Instruction List - @@instruction_list@@
 Suggested Substitution - @@gt_sub@@
 
 
-MC @@sample_id@@-fit. @@Q_FIT_TEXT@@
+MC@@sample_id@@fit. @@Q_FIT_TEXT@@
 
 yes
 no
 
 
-MC @@sample_id@@-change. @@Q_MAJOR_CHANGE_TEXT@@
+MC@@sample_id@@change. @@Q_MAJOR_CHANGE_TEXT@@
 
 yes
 no
 
 
-MC @@sample_id@@-selection. @@Q_SUBS_SELECTION@@
+MC@@sample_id@@selection. @@Q_SUBS_SELECTION@@
 
 @@substitute_suggestion_1@@
 @@substitute_suggestion_2@@
 @@substitute_suggestion_3@@
+"""
+
+    TEXT_CONTENT_ADVANCED = """[[AdvancedFormat]]
+
+[[Block:@@sample_id@@]]
+
+[[Question:Text]]
+Title - @@recipe_title@@
+
+[[Question:Text]]
+Ingredient List - @@ingredient_list@@
+
+
+[[Question:Text]]
+Instruction List - @@instruction_list@@
+
+
+[[Question:Text]]
+Suggested Substitution - (@@gt_sub@@)
+
+[[Question:MC:SingleAnswer:Horizontal]]
+[[ID:MC@@sample_id@@fit]]
+@@Q_FIT_TEXT@@
+
+[[Choices]]
+yes
+no
+
+[[Question:MC:SingleAnswer:Horizontal]]
+[[ID:MC@@sample_id@@change]]
+@@Q_MAJOR_CHANGE_TEXT@@
+
+[[Choices]]
+yes
+no
+
+[[Question:MC:SingleAnswer:Horizontal]]
+[[ID:MC@@sample_id@@selection]]
+@@Q_SUBS_SELECTION@@
+
+[[Choices]]
+@@substitute_suggestion_1@@
+@@substitute_suggestion_2@@
+@@substitute_suggestion_3@@
+other
+
+[[Question:TE]]
+[[ID:MC@@sample_id@@usersuggestion]]
+What Substitution do you suggest instead (if you answered other)?
 """
 
     def __init__(self, gt_sub: list,
@@ -44,20 +93,22 @@ MC @@sample_id@@-selection. @@Q_SUBS_SELECTION@@
                  recipe_ingredient_list: list,
                  recipe_instruction_list: list,
                  sub_options_list: list[str],
-                 sample_id: int) -> None:
+                 sample_id: int,
+                 do_use_advanced: bool) -> None:
         self.gt_sub = [ingr.replace("_", " ") for ingr in gt_sub]
         self.recipe_title = recipe_title
         self.recipe_ingredient_list = [ingr.replace("_", " ") for ingr in recipe_ingredient_list]
         self.recipe_instruction_list = recipe_instruction_list
         self.sub_options_list = sub_options_list
         self.sample_id = sample_id
+        self.do_use_advanced = do_use_advanced
 
     def __str__(self) -> str:
-        text_content = self.TEXT_CONTENT
+        text_content = self.TEXT_CONTENT_ADVANCED if self.do_use_advanced else self.TEXT_CONTENT
         text_content = text_content.replace("@@sample_id@@", str(self.sample_id)) \
                                    .replace("@@recipe_title@@", self.recipe_title) \
-                                   .replace("@@ingredient_list@@", ",\n".join(self.recipe_ingredient_list)) \
-                                   .replace("@@instruction_list@@", ",\n".join(self.recipe_instruction_list)) \
+                                   .replace("@@ingredient_list@@", ";\n".join(self.recipe_ingredient_list)) \
+                                   .replace("@@instruction_list@@", ";\n".join(self.recipe_instruction_list)) \
                                    .replace("@@gt_sub@@", " -> ".join(self.gt_sub)) \
                                    .replace("@@Q_FIT_TEXT@@", self.Q_MAJOR_CHANGE_TEXT) \
                                    .replace("@@Q_MAJOR_CHANGE_TEXT@@", self.Q_MAJOR_CHANGE_TEXT) \
@@ -68,7 +119,7 @@ MC @@sample_id@@-selection. @@Q_SUBS_SELECTION@@
 
         return text_content
 
-def createQualtrixSurveyText(survey_data):
+def createQualtrixSurveyText(survey_data, do_use_advanced):
 
     survey_string = ""
 
@@ -78,7 +129,8 @@ def createQualtrixSurveyText(survey_data):
                                                              recipe["original_ingredients"],
                                                              recipe["instructions"],
                                                              ["A", "B", "C"],
-                                                             sample_id)
+                                                             sample_id,
+                                                             do_use_advanced)
 
         survey_string += str(question_block) + "\n\n"
 
@@ -97,7 +149,7 @@ if __name__ == "__main__":
     with open(QUALTRICS_SURVEY_DATA_FILE_PATH, "r") as qualstrics_data_file:
         survey_data = json.load(qualstrics_data_file)
 
-    survey_text_content = createQualtrixSurveyText(survey_data)#.replace("\\n", "\n").replace("\n", "\n")
+    survey_text_content = createQualtrixSurveyText(survey_data, True)#.replace("\\n", "\n").replace("\n", "\n")
 
     if not DRY_RUN:
         with open(QUALTRICS_SURVEY_TEXT_FILE_PATH, "w") as qualtrics_survey_file:
